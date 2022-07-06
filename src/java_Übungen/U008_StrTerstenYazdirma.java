df46ae1490715fbb8fc049bf5ed255c88475a3cb
Mustafa Celik <mstafa.celik@gmail.com>
2022-07-06 23:34:30 +0200

    day06

---
+++ src/java_Übungen/U008_StrTerstenYazdirma.java
@@ -0,0 +1,29 @@
+package java_Übungen;
+
+import java.util.Scanner;
+
+public class U008_StrTerstenYazdirma {
+
+
+    public static void main(String[] args) {
+
+     /*
+        Girilen String değerde tersten yazan Java kodunu yazınız.
+
+        Test Data:
+        java
+                avaj
+    */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen bir kelime giriniz : ");
+        String str = scan.nextLine();
+
+        String arr[] = str.split("");
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+
+    }
+}
