a42611881c17d847d78682ca2dc196a0627cfef6
Mustafa Celik <mstafa.celik@gmail.com>
2022-07-07 23:54:01 +0200

    day07

---
+++ src/java_Übungen/Ü008_StrConcat.java
@@ -0,0 +1,28 @@
+package java_Übungen;
+
+import java.util.Scanner;
+
+public class Ü008_StrConcat {
+    public static void main(String[] args) {
+
+    /*
+    2 farklı String'i  ucuca ekleyen Java Kodunu yazınız.
+
+    Test Data:
+    str1: Java is
+    str2: fun
+
+    Beklenen Çıktı:
+    java is fun
+
+    */
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen bir kelime giriniz : ");
+        String str1 = scan.nextLine();
+        String str2 = scan.nextLine();
+
+        System.out.println(str1 + " ".concat(str2));
+
+
+    }
+}
